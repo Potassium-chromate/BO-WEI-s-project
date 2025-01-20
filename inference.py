5bce5e73f62a99a358e1d87a78916b4155d7565e
Potassium-chromate <112847018+Potassium-chromate@users.noreply.github.com>
2025-01-20 16:15:36 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,20 +1,23 @@
 from ultralytics import YOLO
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     model = YOLO('runs/detect/train/weights/best.pt')
+    image_path = "datasets/test/images/291203_006201.tif"
+    ground_truth = "datasets/test/labels/291203_006201.txt"
     
     # Perform inference
-    results = model.predict(source='沉沉291203/291203_006001.tif', save=True)
+    results = model.predict(source = image_path, save=True)
     
-    # Get the image with detections
-    image_path = 'test/images/291203_006201.tif'
     image = Image.open(image_path).convert('RGB')  # Convert the image to RGB
     
     # Plot the image with predictions
     fig = plt.figure(figsize=(10, 10))
     plt.imshow(image)
+    predict_loc = []
+    correct_loc = []
     
     # Accessing the results for plotting bounding boxes
     for result in results:
@@ -24,6 +27,7 @@ if __name__ == '__main__':
         for box in boxes:
             # Ensure the box is on the CPU and converted to NumPy
             x1, y1, x2, y2 = box.cpu().numpy()  # Convert to NumPy array
+            predict_loc.append([x1, y2, x2, y2])
             plt.gca().add_patch(plt.Rectangle((x1, y1), x2 - x1, y2 - y1, 
                                               linewidth=2, edgecolor='r', facecolor='none'))
     
@@ -31,3 +35,18 @@ if __name__ == '__main__':
     plt.axis('off')
     plt.savefig('output_image.jpg', bbox_inches='tight', pad_inches=0) 
     plt.show()
+    
+    # Read the ground thuth location:
+    with open(ground_truth, 'r') as f:
+        for line in f:
+            parts = line.split()
+            part_float = [float(parts[0]), float(parts[1]), float(parts[2]), float(parts[4])]
+            correct_loc.append(part_float)
+    
+    
+    predict_loc = np.array(predict_loc)
+    correct_loc = np.array(correct_loc)
+    
+    predict_loc = np.sort(predict_loc, axis = 0)
+    correct_loc = np.sort(correct_loc, axis = 0)
+    
